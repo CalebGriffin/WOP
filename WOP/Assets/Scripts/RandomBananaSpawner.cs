77d7c49367da6e52592c62d6ebf28dad26d39690
LJEvans2001 <75310596+LJEvans2001@users.noreply.github.com>
2021-05-28 16:28:42 +0100

    Room 2 Animation and spawner

---
+++ WOP/Assets/Scripts/RandomBananaSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBananaSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
+    private int rand;
+
+    public float startTimeBtwSpawns;
+    private float timeBtwSpawns;
+
+    void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[0], spawnPoints[randSpawPoint].position, transform.rotation);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
