f16de4be61e27456ff3d7ee69be00e509e3d6cc9
LJEvans2001 <75310596+LJEvans2001@users.noreply.github.com>
2021-06-02 17:47:44 +0100

    Room 2 froggy AI

---
+++ WOP/Assets/Scripts/AIEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIEnemy : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+
+    public LayerMask banana;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Banana").transform;
+    }
+    private void Update()
+    {
+        anim.SetBool("isMoving", isInChaseRange);
+
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, banana);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, banana);
+
+        dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        dir.Normalize();
+        movement = dir;
+        if (shouldRotate)
+        {
+            anim.SetFloat("x", dir.x);
+            anim.SetFloat("Y", dir.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isInChaseRange && !isInAttackRange)
+        {
+            MoveCharacter(movement);
+        }
+        if (isInAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+}
