ab00a840c4101ad9c9773b83dc49f3d1b587eb2e
LJEvans2001 <75310596+LJEvans2001@users.noreply.github.com>
2021-06-02 15:19:19 +0100

    Room 2 Net

---
+++ WOP/Assets/Scripts/Netpickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Netpickup : MonoBehaviour
+{
+    public NetSpawner NetSpawner;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == "Net")
+        {
+            Destroy(other.gameObject);
+            NetSpawner.Setup();
+        }
+    }
+}
