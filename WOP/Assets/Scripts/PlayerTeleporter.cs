ab55564f84fbbf1573057b8c6c5693de040d13dc
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2021-06-07 00:04:49 +0100

    Updated the player portals

--- WOP/Assets/Scripts/PlayerTeleporter.cs
+++ WOP/Assets/Scripts/PlayerTeleporter.cs
@@ -14,37 +14,25 @@ public class PlayerTeleporter : MonoBehaviour
                 SceneManager.LoadScene("Room 1");
                 break;
 
-            case "Room 1":
+            case "Room 2":
                 SceneManager.LoadScene("Room 1");
                 break;
             
             case "Menu":
                 SceneManager.LoadScene("Menu");
                 break;
-        }
-        if (collision.gameObject.tag == "Room 1")
-        {
-            SceneManager.LoadScene("Room 1");
-        }
-
-        if (collision.gameObject.tag == "Room 2")
-        {
-            SceneManager.LoadScene("Room 2");
-        }
-
-        if (collision.gameObject.tag == "Settings")
-        {
-            // Here we will make the game open the settings menu
-        }
-
-        if (collision.gameObject.tag == "Controls")
-        {
-            // Here we will make the game show the controls options to the player
-        }
-
-        if (collision.gameObject.tag == "Credits")
-        {
-            // Here we will have a shameless plug of our names and how we have contributed to this project
+            
+            case "Settings":
+                break;
+            
+            case "Controls":
+                break;
+            
+            case "Credits":
+                break;
+            
+            default:
+                break;
         }
     }
 }
